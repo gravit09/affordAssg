677d266832065774ade7ccb7f10b2f6bad0b449c
Gravit09 <garvitchoudhary09@gmail.com>
2025-04-12 00:40:13 +0530

    shadcn added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
       <h1 className="text-lg text-center">empty app</h1>
+      <Button>Test</Button>
     </div>
   );
 }
